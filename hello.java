b47d371c5ec9fffbce914a36b8e91149d7d735b3
Dinhisme <Phungquocvinh15112003@gmail.com>
2025-11-23 15:33:01 +0700

    push git baitap 2

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello{
 
     public static void main (String[] args){
 
-        System.out.println("Hello World da xuong dong");
+        System.out.println("Hello World da xuong dong baitap 2");
     }
 
 }
